1c6543e0a17a70d243a3e17921c8a4ecb5edebbf
aagamsalot1996@gmail.com <aagamsalot1996@gmail.com>
2022-07-20 19:35:02 +0530

    Added NoBlock in ZMQ

--- src/main/java/ZMQPushMany.java
+++ src/main/java/ZMQPushMany.java
@@ -6,13 +6,18 @@ public class ZMQPushMany {
             for (int j = 0; j < 3; j++) {
                 new Thread(new Runnable() {
                     public void run() {
-                        ZMQ.Context context = ZMQ.context(1);
+                        ZMQ.Context context = ZMQ.context(3);
                         ZMQ.Socket push = context.socket(ZMQ.PUSH);
-                        push.setSndHWM(0);
+//                        push.setSndHWM(0);
+//                        push.setSendBufferSize(1024*2);
                         push.connect("tcp://localhost:7070");
                         for (int i = 0; i < 2000; i++) {
-                            push.send(("hello "+i).getBytes());
-                            System.out.println(i);
+                            if(push.send(("hello "+i).getBytes(),ZMQ.NOBLOCK)){
+                                System.out.println(i);
+                            }else{
+                                System.out.println("ERROR----");
+                            }
+
                             //blocked after a while if Pull is not available at moment
                             // (store the data in buffer,pull will use it once it's available)
                         }
