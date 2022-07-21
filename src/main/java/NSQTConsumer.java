5d166bd667384462c37516e14980d39fe1117846
aagamsalot1996@gmail.com <aagamsalot1996@gmail.com>
2022-07-21 18:34:47 +0530

    Added multi Consumer based on executor

--- src/main/java/NSQTConsumer.java
+++ src/main/java/NSQTConsumer.java
@@ -3,16 +3,22 @@ import com.github.brainlag.nsq.NSQMessage;
 import com.github.brainlag.nsq.callbacks.NSQMessageCallback;
 import com.github.brainlag.nsq.lookup.DefaultNSQLookup;
 import com.github.brainlag.nsq.lookup.NSQLookup;
+import io.netty.util.concurrent.SingleThreadEventExecutor;
+import zmq.socket.pubsub.XPub;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NSQTConsumer extends  Thread{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         NSQLookup nsqLookup = new DefaultNSQLookup();
         nsqLookup.addLookupAddress("localhost", 4161);
-        for(int i=0;i<5;i++){
+        for(int i=0;i<1;i++){
             NSQTConsumer nsqtConsumer = new NSQTConsumer();
-            nsqtConsumer.setName("Thread "+i);
+            nsqtConsumer.setName("Thread_"+i);
+            Thread.sleep(100);
             nsqtConsumer.start();
         }
     }
@@ -23,6 +29,7 @@ public class NSQTConsumer extends  Thread{
         NSQLookup nsqLookup = new DefaultNSQLookup();
         nsqLookup.addLookupAddress("localhost", 4161);
         String s1 = Thread.currentThread().getName();
+        System.out.println("name of the thread==="+s1);
         NSQMessageCallback nsqMessageCallback = new NSQMessageCallback() {
             @Override
             public void message(NSQMessage message) {
@@ -37,7 +44,16 @@ public class NSQTConsumer extends  Thread{
                 }
             }
         };
-        NSQConsumer consumer1 = new NSQConsumer(nsqLookup, "test1", "channel1", nsqMessageCallback);
+        NSQConsumer consumer1 = new NSQConsumer(nsqLookup, "test1", "channel_11", nsqMessageCallback);
+        consumer1.setExecutor(Executors.newFixedThreadPool(6));
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        executorService.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("hello...............");
+//            }
+//        });
+//        consumer1.setExecutor(executorService);
         consumer1.start();
     }
 }
